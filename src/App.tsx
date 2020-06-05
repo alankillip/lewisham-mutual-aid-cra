3a6abe1f34890f0b01550987727cbcb1dce84664
alankillip <alan.kilip@dentsuaegis.com>
2020-06-05 20:50:27 +0100

    custom links

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  useHistory
 } from "react-router-dom";
 import {menu} from './Routes';
 import {RouteItem} from "./models-app/Route";
 
-
 const page = (route: RouteItem, index: number) => {
   const RouteComponent = route.component;
-  console.log('route.component', route.component);
   return (
     <Route exact={index === 0} path={route.path} key={route.path}>
       <RouteComponent />
@@ -19,18 +17,26 @@ const page = (route: RouteItem, index: number) => {
   );
 };
 
+const MenuItem: React.FunctionComponent<RouteItem> = (route: RouteItem) => {
+  let history = useHistory();
+
+  const handleClick = (path: string) => () => {
+    history.push(path);
+  }
+
+  return (
+    <div onClick={handleClick(route.path)} key={route.path}>
+      {route.label}
+    </div>
+  );
+};
+
+
 export default function App() {
   return (
     <Router>
       <div>
-        <ul>
-          {menu.map((route: RouteItem) => (
-            <li key={route.path}>
-              <Link to={route.path}>{route.label}</Link>
-            </li>
-          ))}
-        </ul>
-        <hr />
+        {menu.map((route: RouteItem) => <MenuItem {...route} />)}
         <Switch>
           {menu.map(page)}
         </Switch>
