53ef456b5d469ff3f8264e8cfe5b0b5e2870899c
alankillip <alan.kilip@dentsuaegis.com>
2020-06-13 10:02:04 +0100

    rudimentry search box

--- src/pages/resources/search.tsx
+++ src/pages/resources/search.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Search = () => (
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setSearchTerm(e.currentTarget.value);
+  }
+  return (
   <div>
-    Lewisham Mutual Aid Search
+    <div>Search<input type="text" onChange={onChange} /></div>
+    <div>{searchTerm}</div>
   </div>
-);
+)};
 
 export default Search;
