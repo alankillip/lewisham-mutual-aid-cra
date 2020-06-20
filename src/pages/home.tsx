1000a975870890406b8d995cba77400efd68697a
alankillip <alan.kilip@dentsuaegis.com>
2020-06-20 11:18:31 +0100

    populate home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => (
-  <div>
-    Lewisham Mutual Aid Home
+  <div className="page">
+    <p>Hi! Thanks for visiting the Lewisham COVID-19 Mutual Aid group website.</p>
+    <p>If you're looking for helpful organisations, you can browse the resources menu, or you can search resources by clicking <a href="resources/search-resources">here</a>.</p>
+    <p>If you're looking for a group to join, please click <a href="find-a-group/search-locally">here</a>.</p>
+
+
+    <h2>What is mutual aid?</h2>
+    <p>A voluntary reciprocal exchange of resources and services for mutual benefit. Based on non-hierarchical principles. We want to support each other and not wait for central government to get their act together.</p>p
   </div>
 );
 
