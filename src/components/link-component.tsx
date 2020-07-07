96daf4e68ff4aee08746741647295b5110b9e7a2
alankillip <alan.kilip@dentsuaegis.com>
2020-07-07 10:06:53 +0100

    Add new link category

--- src/components/link-component.tsx
+++ src/components/link-component.tsx
@@ -6,16 +6,24 @@ import {ExternalLink} from './resource-parts';
 const LinkComponent = (props: LinkComponentProps) => {
   const {links} = props;
   const generalLinks = links.filter(({type}: Link) => type === 'general');
+  const toolsLinks = links.filter(({type}: Link) => type === 'tools');
   const charityLinks = links.filter(({type}: Link) => type === 'charity');
   return (
     <div>
-      {links.length > 0 && <div className="title">General Links:</div>}
+      {links.length > 0 && <div className="title">General Advice and Guidance:</div>}
       {generalLinks.map(
       (link: Link, index: number) => (
         <div key={index} className="content-card">
           <ExternalLink url={link.link} text={link.name} />
         </div>
       ))}
+      {toolsLinks.length > 0 && <div className="title">Mutual Aid Volunteer Tools:</div>}
+      {charityLinks.map(
+        (link: Link, index: number) => (
+          <div key={index} className="content-card">
+            <ExternalLink url={link.link} text={link.name} />
+          </div>
+        ))}
       {links.length > 0 && <div className="title">Charity Links:</div>}
       {charityLinks.map(
         (link: Link, index: number) => (
