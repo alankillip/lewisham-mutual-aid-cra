bdf12b6a2825ca1f1451aa7f968003fdb3715c2a
alankillip <alan.kilip@dentsuaegis.com>
2020-07-08 22:24:03 +0100

    default to lewisham

--- src/pages/groups/global/index.tsx
+++ src/pages/groups/global/index.tsx
@@ -6,7 +6,7 @@ const Global = () => (
       width="100%"
       height="1000"
       title="mutual-aid-wiki"
-      src="https://mutualaid.wiki"
+      src="https://mutualaid.wiki?lewisham"
     />
   </div>
 );
