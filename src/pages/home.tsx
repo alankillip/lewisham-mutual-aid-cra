30fb33a6b9d6f0ee82992015e944aa25d25315ab
alankillip <alan.kilip@dentsuaegis.com>
2020-07-31 12:53:24 +0100

    further tweaks

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import Search from "./resources/search";
 
 const Home = () => (
   <div className="page">
-    <p>If you're looking for help, please email <a href="mailto:lewisham.mutualaid@gmail.com">lewisham.mutualaid@gmail.com</a></p>
+    If you're looking for help, please email <a href="mailto:lewisham.mutualaid@gmail.com">lewisham.mutualaid@gmail.com</a>
     <p>Or, you can find your local group by clicking <a href="find-a-group">here</a>.</p>
     <p>If you're looking for helpful organisations, you can browse the resources menu, or you can search resources by typing something into this box:</p>
     <Search />
